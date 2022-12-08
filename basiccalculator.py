755d7d74859780cacfb417bb61f4601872b70cf1
Arthur Formiga <arthurformigademelo2@gmail.com>
2022-12-08 10:22:46 -0300

    Corrigido

--- basiccalculator.py
+++ basiccalculator.py
@@ -16,28 +16,34 @@ def multi(n1, n2):
     answer = n1 * n2
     print(str(n1) + " x " + str(n2) + " = " + str(answer))
 
-x = input('a- Adição\nb- Subtração\nc- Multiplicação\nd- Divisão\nEscolha uma função: ')
+while True:
 
-if x == 'a' or x == "A":
-    print('Soma')
-    n1 = int(input('Digite o primeiro numero: '))
-    n2 = int(input('Digite o segundo numero: '))
-    add(n1, n2)
+    x = input('\na- Adição\nb- Subtração\nc- Multiplicação\nd- Divisão\ne- Finalizar\n Escolha uma função: ')
 
-if x == 'b' or x== "B":
-    print('Subtração')
-    n1 = int(input('Digite o primeiro numero: '))
-    n2 = int(input('Digite o segundo numero: '))
-    sub(n1, n2)
+    if x == 'a' or x == "A":
+        print('Soma')
+        n1 = int(input('Digite o primeiro numero: '))
+        n2 = int(input('Digite o segundo numero: '))
+        add(n1, n2)
 
-if x == 'c' or x == "C":
-    print('Multiplicação')
-    n1 = int(input('Digite o primeiro numero: '))
-    n2 = int(input('Digite o segundo numero: '))
-    multi(n1, n2)
+    if x == 'b' or x== "B":
+        print('Subtração')
+        n1 = int(input('Digite o primeiro numero: '))
+        n2 = int(input('Digite o segundo numero: '))
+        sub(n1, n2)
 
-if x == 'd' or x == "D":
-    print('Divisão')
-    n1 = int(input('Digite o primeiro numero: '))
-    n2 = int(input('Digite o segundo numero: '))
-    div(n1, n2)
+    if x == 'c' or x == "C":
+        print('Multiplicação')
+        n1 = int(input('Digite o primeiro numero: '))
+        n2 = int(input('Digite o segundo numero: '))
+        multi(n1, n2)
+
+    if x == 'd' or x == "D":
+        print('Divisão')
+        n1 = int(input('Digite o primeiro numero: '))
+        n2 = int(input('Digite o segundo numero: '))
+        div(n1, n2)
+
+    if x == 'e' or x == 'E':
+        print('Fim de programa')
+        quit()
